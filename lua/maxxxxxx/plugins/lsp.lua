78eb8194fcb6102d973a1da6648facf468289c13
maxxxxx <maxxng1028@gmail.com>
2024-01-29 22:11:45 +0800

    feat: added capabilities and on_attach to lsp setup

--- lua/maxxxxxx/plugins/lsp.lua
+++ lua/maxxxxxx/plugins/lsp.lua
@@ -14,21 +14,39 @@ return {
     },
     config = function()
         local cmp = require("cmp")
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        local on_attach = function(_, bufnr)
+            local opts = { buffer = bufnr, remap = false }
+
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+            vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+            vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+            vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+            vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+            vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+            vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
+        end
+
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "clangd",
-                "clang-format",
-                "cmake",
                 "lua_ls",
                 "rust_analyzer",
                 "tsserver",
-                "stylua",
             },
             handlers = {
                 function(server_name)
-                    require("lspconfig")[server_name].setup({})
+                    lspconfig[server_name].setup({
+                        on_attach = on_attach,
+                        capabilities = capabilities
+                    })
                 end,
 
                 ["lua_ls"] = function()
@@ -42,7 +60,7 @@ return {
                             }
                         }
                     })
-                end
+                end,
             }
         })
 
