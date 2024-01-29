6631490f95184dac0c0eb23b717ca9c6bd80c11c
maxxxxx <maxxng1028@gmail.com>
2024-01-29 22:38:04 +0800

    style: changed on_attach function declaration

--- lua/maxxxxxx/plugins/lsp.lua
+++ lua/maxxxxxx/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
         local lspconfig = require("lspconfig")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local on_attach = function(_, bufnr)
+        local function on_attach(_, bufnr)
             local opts = { buffer = bufnr, remap = false }
 
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
