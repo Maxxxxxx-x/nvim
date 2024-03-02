0b7cc5f65662a2924729d2bb5c1941944546913f
maxxxxx <maxxng1028@gmail.com>
2024-03-02 21:45:02 +0800

    feat: changed file name and output dir

--- lua/maxxxxxx/plugins/silicon.lua
+++ lua/maxxxxxx/plugins/silicon.lua
@@ -5,9 +5,16 @@ return {
     config = function()
         require("silicon").setup({
             font = "CascadiaCode=32",
-            theme = "gruvbox",
             tab_width = 4,
-            to_clipboard = true,
+            output = function()
+                local path = "/mnt/c/Users/user/Pictures/silicon-nvim/"
+                local file_name = vim.fn.expand("%:t")
+                local date_string= os.date("!%Y-%m-%dT%H-%M-%S")
+                return string.format("%s%s_%s.png", path, date_string, file_name)
+            end,
+            window_title = function()
+                return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+            end,
         })
     end
 }
