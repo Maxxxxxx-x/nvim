256acb4f56f37ae30ca25d902a4a1ac2f2b1ef31
maxxxxx <maxxng1028@gmail.com>
2024-03-03 14:47:30 +0800

    feat: updated font size, file_name

--- lua/maxxxxxx/plugins/silicon.lua
+++ lua/maxxxxxx/plugins/silicon.lua
@@ -4,17 +4,17 @@ return {
     cmd = "Silicon",
     config = function()
         require("silicon").setup({
-            font = "CascadiaCode=32",
+            font = "CascadiaCode=26",
             tab_width = 4,
             output = function()
                 local path = "/mnt/c/Users/user/Pictures/silicon-nvim/"
-                local file_name = vim.fn.expand("%:t")
+                local file_name = vim.fn.expand("%s")
                 local date_string= os.date("!%Y-%m-%dT%H-%M-%S")
                 return string.format("%s%s_%s.png", path, date_string, file_name)
             end,
             window_title = function()
                 return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
-            end,
+            end
         })
     end
 }
