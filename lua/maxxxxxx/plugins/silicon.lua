ac42f32477a8d0f75f663ec96d5efff7a18b7f9a
maxxxxx <maxxng1028@gmail.com>
2024-03-03 22:23:17 +0800

    refactor: removed file_name from screenshot file name

--- lua/maxxxxxx/plugins/silicon.lua
+++ lua/maxxxxxx/plugins/silicon.lua
@@ -8,9 +8,8 @@ return {
             tab_width = 4,
             output = function()
                 local path = "/mnt/c/Users/user/Pictures/silicon-nvim/"
-                local file_name = vim.fn.expand("%s")
                 local date_string= os.date("!%Y-%m-%dT%H-%M-%S")
-                return string.format("%s%s_%s.png", path, date_string, file_name)
+                return string.format("%s%s.png", path, date_string)
             end,
             window_title = function()
                 return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
