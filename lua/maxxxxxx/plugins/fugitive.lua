7781c10e69cdee6645fd65f3ab280df9e598e8e6
maxxxx <maxxng1028@gmail.com>
2024-03-04 12:23:47 +0800

    feat: new keybinds for windows

--- lua/maxxxxxx/plugins/fugitive.lua
+++ lua/maxxxxxx/plugins/fugitive.lua
@@ -2,5 +2,16 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>ga", vim.cmd("Gwrite %"))
+        vim.keymap.set("n", "<leader>gc", function()
+            local commitMessage = vim.fn.input("Commit message: ")
+            return vim.cmd(string.format("Git commit %s -m %s", "%", commitMessage))
+        end)
+        vim.keymap.set("n", "<leader>grm", vim.cmd("Git rm %"))
+        vim.keymap.set("n", "<leader>gr", function()
+            local remote = vim.fn.input("Remote: ")
+            local branch = vim.fn.input("Branch: ")
+            return vim.cmd(string.format("Git push %s %s", remote, branch))
+        end)
     end
 }
