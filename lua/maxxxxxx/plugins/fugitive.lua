6d664e36891dcaece147b6f0d8870e24a0aec440
Maxxxxxx-x <maxxng1028@gmail.com>
2024-03-04 19:01:49 +0800

    feat: new keybind for git diff

--- lua/maxxxxxx/plugins/fugitive.lua
+++ lua/maxxxxxx/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
         vim.keymap.set("n", "<leader>ga", function()
             return vim.cmd("Gwrite %")
         end)
