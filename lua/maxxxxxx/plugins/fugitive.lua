09dfb5cc2a3f2f8382f92cfac065b3ea7bc0feb7
Maxxxxxx-x <maxxng1028@gmail.com>
2024-03-04 11:18:40 +0800

    feat: new commands for git add and git commit

--- lua/maxxxxxx/plugins/fugitive.lua
+++ lua/maxxxxxx/plugins/fugitive.lua
@@ -1,6 +1,17 @@
 return {
     "tpope/vim-fugitive",
     config = function()
+        local function getFilePath()
+            print(vim.fn.expand("%s"))
+            return vim.fn.expand("%s");
+        end
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>ga", function()
+            return vim.cmd("Gwrite %")
+        end)
+        vim.keymap.set("n", "<leader>gc", function()
+            local commitMessage = vim.fn.input("Commit message: ")
+            return vim.cmd(string.format('Git commit %s -m "%s"', "%", commitMessage))
+        end)
     end
 }
