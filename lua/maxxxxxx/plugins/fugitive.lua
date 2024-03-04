046492f6634ad17ee661148f1ad111e9e625b46c
Maxxxxxx-x <maxxng1028@gmail.com>
2024-03-04 11:21:21 +0800

    feat: new keybinds for add, commit and remove

--- lua/maxxxxxx/plugins/fugitive.lua
+++ lua/maxxxxxx/plugins/fugitive.lua
@@ -1,10 +1,6 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        local function getFilePath()
-            print(vim.fn.expand("%s"))
-            return vim.fn.expand("%s");
-        end
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         vim.keymap.set("n", "<leader>ga", function()
             return vim.cmd("Gwrite %")
@@ -12,6 +8,9 @@ return {
         vim.keymap.set("n", "<leader>gc", function()
             local commitMessage = vim.fn.input("Commit message: ")
             return vim.cmd(string.format('Git commit %s -m "%s"', "%", commitMessage))
+        end)
+        vim.keymap.set("n", "<leader>grm", function()
+            return vim.cmd("Git rm %")
         end)
     end
 }
