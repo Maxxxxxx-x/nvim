db13d46e9ad82b3564e68aa107e46185bb9792f2
Maxxxxxx-x <maxxng1028@gmail.com>
2026-05-10 12:35:06 +0800

    feat: added keybinds to cloak toggle and cloak preview

--- lua/maxxxxxx/plugins/cloak.lua
+++ lua/maxxxxxx/plugins/cloak.lua
@@ -12,9 +12,12 @@ return {
                         "wrangler.toml",
                         ".dev.vars",
                     },
-                    cloak_pattern = "=.+"
-                }
-            }
+                    cloak_pattern = "=.+",
+                },
+            },
         })
-    end
+
+        vim.keymap.set("n", "<leader>ct", "<cmd>CloakToggle<cr>", { desc = "[C]loak [T]oggle" })
+        vim.keymap.set("n", "<leader>cp", "<cmd>CloakPreviewLine<cr>", { desc = "[C]loak [P]review Line" })
+    end,
 }
